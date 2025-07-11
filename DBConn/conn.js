c4dd934fab01bd09fe95fc24f748052aeb7d4a4a
Sushant-yadav1s <sy032686@gmail.com>
2025-07-11 11:52:37 +0530

    Update conn.js

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+
+const uri = "mongodb+srv://sy032686:P3Bqbz5mHtgsLF5C@cluster0.4wvaubg.mongodb.net/gymBackend?retryWrites=true&w=majority&appName=Cluster0";
+
 mongoose
-  .connect("mongodb://localhost:27017/gymBackend")
+  .connect(uri, {
+    // no need for useNewUrlParser/useUnifiedTopology now, latest mongoose defaults handle it
+  })
   .then(() => {
-    console.log("DB connection successfull");
+    console.log("DB connection successful");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("DB connection error:", err);
   });
